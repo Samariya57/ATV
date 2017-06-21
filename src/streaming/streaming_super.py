8acf3c71372cec3ac73d0d4d43ebffadd966a226
Mariia Danilenko <Samariya57@gmail.com>
2017-06-21 14:57:07 -0400

    Update streaming_super.py

--- src/streaming/streaming_super.py
+++ src/streaming/streaming_super.py
@@ -2,12 +2,14 @@ import threading, logging, time
 import random
 import json
 import MySQLdb
-from kafka import KafkaConsumer
-import gc
 import redis
 import time
 import datetime
+from kafka import KafkaConsumer
 from kafka import KafkaProducer
+
+# This script is a Kafka consumer for topic "transactions". It checks connection degree and send new messages with
+# new topics "True" or "False" to server as a Kafka Producer for next data writings
 
 class Streaming(threading.Thread):
     daemon = True
@@ -16,25 +18,28 @@ class Streaming(threading.Thread):
     def __init__(self):
         super(Streaming, self).__init__()
 	
-	self.db = MySQLdb.connect(host="ec2-54-158-19-194.compute-1.amazonaws.com", user="venmo", passwd="pass", db="VenmoDB")
-	self.rediska = redis.StrictRedis(host='ec2-34-207-202-197.compute-1.amazonaws.com', port=6379, db=0)
-	self.rediska_from = redis.StrictRedis(host='ec2-34-207-202-197.compute-1.amazonaws.com', port=6379, db=1)
-	self.rediska_to = redis.StrictRedis(host='ec2-34-207-202-197.compute-1.amazonaws.com', port=6379, db=2)
+	self.db = MySQLdb.connect(host="ec2-yy-xxx-yy-xxx.compute-1.amazonaws.com", user="user", passwd="password", db="VenmoDB")
+	self.rediska = redis.StrictRedis(host='ec2-xx-xxx-xx-xxx.compute-1.amazonaws.com', port=6379, db=0)
+	self.rediska_from = redis.StrictRedis(host='ec2-xx-xxx-xx-xxx.compute-1.amazonaws.com', port=6379, db=1)
+	self.rediska_to = redis.StrictRedis(host='ec2-xx-xxx-xx-xxx.compute-1.amazonaws.com', port=6379, db=2)
+    
     def __filter_nones__(self,transaction_data):
     	if transaction_data is not None:
         	return True
     	return False
         
     def run(self):
-        consumer = KafkaConsumer(bootstrap_servers='34.226.228.0:9092',group_id="gruppa")
+        consumer = KafkaConsumer(bootstrap_servers='xx.xxx.xxx.xxx:9092',group_id="gruppa")
         consumer.subscribe(['transactions'])
-	producer = KafkaProducer(bootstrap_servers='34.226.228.0:9092')
+	producer = KafkaProducer(bootstrap_servers='xx.xxx.xxx.xxx:9092')
         for message in consumer:
             msg = str(message.value)
             unwraped = self.__extract_data__(msg)
 	    if self.__filter_nones__(unwraped):
-            	result = self.__transaction_between__(unwraped,self.db, self.rediska, self.rediska_from, self.rediska_to)
+            	result = self.__transaction_process__(unwraped,self.db, self.rediska, self.rediska_from, self.rediska_to)
 		producer.send(str(result), message.value)
+    
+    # This function returns True if found zero, first or second degree connection 	
     def __check_friendship__(self,user1,user2,db):
 
         cur = db.cursor()
@@ -71,64 +76,27 @@ class Streaming(threading.Thread):
 
         gc.collect()
         return False
-
 	
-    def __rec_user__(self,ID1, FullName1, ID2, FullName2, db):
-        cur = db.cursor()
-        try:
-                cur.execute("INSERT INTO Users VALUE (%s,%s)", (ID1,FullName1))
-                db.commit()
-                cur.execute("INSERT INTO Users VALUE (%s,%s)", (ID2,FullName2))
-                db.commit()
-	except:
-		cur.close()
-	cur.close()
-
-    
+    # Transform string with transaction info(JSON) to dictionary
     def __extract_data__(self, json_obj):
         json_body = json.loads(json_obj)
     	try:
         	from_id = json_body['actor']['id']
-        #from_firstname = json_body['actor']['firstname']
-        #from_lastname = json_body['actor']['lastname']
-        	#from_username = json_body['actor']['username']
-        	#from_picture = json_body['actor']['picture']
        		is_business = json_body['actor']['is_business']
 
         	to_id = json_body['transactions'][0]['target']['id']
-        #to_firstname = json_body['transactions'][0]['target']['firstname']
-        #to_lastname = json_body['transactions'][0]['target']['lastname']
-        	#to_username = json_body['transactions'][0]['target']['username']
-        	#to_picture = json_body['transactions'][0]['target']['picture']
         	is_business = is_business or json_body['transactions'][0]['target']['is_business']
 
         	if is_business is True:
             		return None
-
-        	# Transaction data
-        	#payment_id = json_body['payment_id']
-        	#message = json_body['message']
-        	#timestamp = json_body['created_time']
     	except:
         	return None
 
 	data = {'from_id': int(from_id),
-        	#'from_firstname': from_firstname,
-            	#'from_lastname': from_lastname,
-            	#'from_username': from_username,
-            	#'from_picture': from_picture,
             	'to_id': int(to_id)}
-            	#'to_firstname': to_firstname,
-           	#'to_lastname': to_lastname,
-            	#'to_username': to_username,
-            	#'to_picture': to_picture,
-           	#'message': message,
-            	#'timestamp': timestamp,
-            	#'payment_id': int(payment_id),
-            	#'amount': random.randint(5,75)}
     	return data
 
-    def __transaction_between__(self,transaction_data, db, rediska, rediska1, rediska2):
+    def __transaction_process__(self,transaction_data, db, rediska, rediska1, rediska2):
 
     
     	user1 = transaction_data['from_id']
