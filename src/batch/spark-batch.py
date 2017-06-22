c98909c996301ba87d2f9c6e1ba363320dada63a
samariya57 <samariya57@gmail.com>
2017-06-21 21:17:31 -0400

    clean batck-spark.py file with comments

--- src/batch/spark-batch.py
+++ src/batch/spark-batch.py
@@ -10,38 +10,26 @@ import sys
 import MySQLdb
 import json
 import gc
-#import redis
-
 from pyspark import SparkContext
 from pyspark import SparkConf
 from pyspark.sql import SparkSession
 from pyspark.streaming import StreamingContext
 from pyspark.streaming.kafka import KafkaUtils
 from botocore.exceptions import ClientError
-#from kafka import KafkaProducer
-#from sets import Set
-# import boto3
 import random
-#import time
-#import datetime
 
+# JSON -> dictionary
 def extract_data(json_body):
 
-    #print("extract_data")
     json_body = json.loads(json_body)
-
 
     try:
         from_id = json_body['actor']['id']
-        #from_firstname = json_body['actor']['firstname']
-        #from_lastname = json_body['actor']['lastname']
         from_username = json_body['actor']['username']
         from_picture = json_body['actor']['picture']
         is_business = json_body['actor']['is_business']
 
         to_id = json_body['transactions'][0]['target']['id']
-        #to_firstname = json_body['transactions'][0]['target']['firstname']
-        #to_lastname = json_body['transactions'][0]['target']['lastname']
         to_username = json_body['transactions'][0]['target']['username']
         to_picture = json_body['transactions'][0]['target']['picture']
         is_business = is_business or json_body['transactions'][0]['target']['is_business']
@@ -57,13 +45,9 @@ def extract_data(json_body):
         return None
 
     data = {'from_id': int(from_id),
-            #'from_firstname': from_firstname,
-            #'from_lastname': from_lastname,
             'from_username': from_username,
             'from_picture': from_picture,
             'to_id': int(to_id),
-            #'to_firstname': to_firstname,
-            #'to_lastname': to_lastname,
             'to_username': to_username,
             'to_picture': to_picture,
             'message': message,
@@ -72,18 +56,18 @@ def extract_data(json_body):
 	    'amount': random.randint(5,75)}
     return data
 
+# Filter nones (transaction with at least one business person)
 def filter_nones(transaction_data):
     if transaction_data is not None:
         return True
     return False
 
+# Check zero degree connection between two users and write them as friends
 def check_friendship(user1,user2,db):
 
-        #db = MySQLdb.connect(host="ec2-54-158-19-194.compute-1.amazonaws.com", user="venmo", passwd="pass", db="VenmoDB")
         cur = db.cursor()
         if (cur.execute("Select * FROM Friends WHERE (ID1,ID2)=(%s, %s)", (user1,user2))>0):
                 cur.close()
-                #db.close()
                 return True
 	try:
         	cur.execute("INSERT INTO Friends VALUE (%s,%s)", (user1,user2))
@@ -92,11 +76,9 @@ def check_friendship(user1,user2,db):
 		cur.close()
         except:
 		cur.close()
-	#if (set(list1).intersection(list2))
-        #db.close()
 	gc.collect()
         return False
-
+# Write new user info into DB (MySQL)
 def rec_user(ID1, FullName1, ID2, FullName2, db):
 	cur = db.cursor()
 	if (cur.execute("Select * FROM Users WHERE ID=%s", (ID1))<1):
@@ -106,11 +88,10 @@ def rec_user(ID1, FullName1, ID2, FullName2, db):
                 cur.execute("INSERT INTO Users VALUE (%s,%s)", (ID2,FullName2))
                 db.commit()
 	cur.close()
-	#gc.collect()
 
+# Transaction processing: check users, check friendship, write transaction into DB
 def transaction_between(transaction_data, db):
 
-    #print("transaction_between")
     user1 = transaction_data['from_id']
     user2 = transaction_data['to_id']
     user1FN = transaction_data['from_username']
@@ -127,25 +108,16 @@ def transaction_between(transaction_data, db):
     try:
 	cur=db.cursor()
 	cur.execute("INSERT INTO Transactions VALUE (%s,%s,%s,%s,%s,%s,%s,%s,%s)",(payment_id, user1,user1FN,user2,user2FN,time_t, message,amount,result))
-	#cur.execute("INSERT INTO Transaction_NT VALUE (%s,%s,%s,%s,%s)",(-payment_id, user2,user1,-amount,result))
 	db.commit()	
-	cur.close()
-	db.close()
     except:
-	cur.close()
-	db.close()
-    gc.collect()
+	db.rollback()
+    db.close()
     return True    
 
 
 # To submit script:
 # $SPARK_HOME/bin/spark-submit --master spark://34.225.200.18:7077 --executor-memory 6G spark_batch.py
 if __name__ == "__main__":
-
-    #db = MySQLdb.connect(host="ec2-54-158-19-194.compute-1.amazonaws.com", user="venmo", passwd="pass", db="VenmoDB")
-    #cur = db.cursor()
-    #print( (cur.execute("Select * FROM Friends_NT WHERE (ID1,ID2)=(%s, %s)", (123,563))))
-    #cur.close()        
 
     list_rez=[]
     sc = SparkContext(appName="Venmo")
@@ -154,11 +126,8 @@ if __name__ == "__main__":
         read_rdd = sc.textFile("s3a://venmo-json/2015_"+str(i).zfill(2)+"/*")
 
         cleaned_rdd = read_rdd.map(lambda x: extract_data(x)).filter(lambda x: filter_nones(x)) # clean json data
-    #print(cleaned_rdd.collect())
-        check_friends = cleaned_rdd.map(lambda x: transaction_between(x,MySQLdb.connect(host="ec2-54-158-19-194.compute-1.amazonaws.com", user="venmo", passwd="pass", db="VenmoDB")))
-    #check_friends.saveAsTextFile("s3a://venmo-json/2012_04.out"+v)
-    #print(check_friends.count())
-    #print(check_friends.countByValue())
+        check_friends = cleaned_rdd.map(lambda x: transaction_between(x,MySQLdb.connect(host="ec2-xx-xx-xx-xxx.compute-1.amazonaws.com", user="user", passwd="password", db="VenmoDB")))
+
         list_rez.append(check_friends.count())
 	gc.collect()
     print(sum(list_rez))
